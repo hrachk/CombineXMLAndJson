8edc5ed89b64df621e9171af80e96851410c5c00
Hrach <hkarap@hotmail.com>
2022-01-13 00:32:35 +0400

    Add project files.

---
+++ CombineXMLAndJson/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using CombineXMLAndJson.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CombineXMLAndJson.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet("get.{format}"), FormatFilter]
+        public IEnumerable<Employee> Get()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { Id = 1, Name = "Sarath Lal", City="Cochin" },
+                new Employee { Id = 2, Name = "Anil Kumar", City="Bangalore"}
+            };
+            return employees;
+        }
+
+        [HttpPost("post.{format}"), FormatFilter]
+        public Employee Post([FromBody] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                return employee;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
